ef8f877a28ab205c07128b65e5efd8a3f7c834b5
Ryan Sidebottom <rmsidebottom0@gmail.com>
2021-02-04 21:19:05 -0800

    Finished setting up tiny url maker

--- urlmonster.py
+++ urlmonster.py
@@ -12,15 +12,7 @@ table = "links"
 user = "root"
 secretFile = "secrets.txt"
 password = Path(secretFile).read_text()[:-1]
-
-''' [
-    {
-      "id": "12345", "long": "https://www.google.com/", "short": "http://localhost/12345"
-    },
-    {
-      "id": "09876", "long": "www.bing.com", "short": "http://localhost/09876"
-    }
-    ]'''
+domain = "localhost:5000/"
 
 '''
 Takes full url and returns the shortened version
@@ -31,12 +23,16 @@ def shorten():
     url = req['url']
     # using sha1 hash, no hash ids function in python
     # get hex digest to shorten hash
-    hash = hashlib.sha1(url.encode("UTF-8")).hexdigest()
+    hash = hashlib.sha1(url.encode("UTF-8")).hexdigest()[0:6]
     # pull first 6 characters to make tiny url
-    return hash[0:6]
+    shorturl = escape(f'{domain}{hash}')
+    sql_statement = f"insert into {table} (hashid, longurl, shorturl) values (\'{hash}\',\'{escape(url)}\', \'{shorturl}\')"
     # push tiny url into database
-    # encode the value before database submission
-    # url = encode(req['url'])
+    ans = runDbQuery(sql_statement)
+    if ans["status"]:
+        return f"Your tinyurl is {shorturl}"
+    else:
+        return ans["message"]
 
 '''
 Takes shortened url and returns the full url
@@ -45,20 +41,52 @@ Takes shortened url and returns the full url
 def lengthen():
     # get the url ID
     id = request.args.get('id')
-    # pull id from database if exists
-    # for itm in urls:
-    #     if itm['id'] == id:
-    #         # return full url
-    #         return itm['long']
-    # return 'error' message to user
-    return "The ID entered does not map to an existing URL"
+
+    # prepare select statement, we know we want to turn the ID into a url
+    sql_statment = f"SELECT longurl from {table} where hashid = \'{id}\'"
+
+    # make the query
+    ans = runDbQuery(sql_statment)
+
+    # check if query succeeded
+    if ans["status"]:
+        # grab url and decode it
+        url = ans["result"][0].decode()
+        # return url to satisfy request
+        return url
+    else:
+        # on failure, return the message
+        return ans["message"]
+
 
 '''
 Takes a short url id and redirects the user to the long url
 '''
 @app.route("/<id>")
 def receiveID(id):
-    # catch errors if possible
+    # prepare select statement, we know we want to turn the ID into a url
+    sql_statment = f"SELECT longurl from {table} where hashid = \'{id}\'"
+    print(sql_statment)
+    # make the query
+    ans = runDbQuery(sql_statment)
+
+    # check if query succeeded
+    if ans["status"]:
+        # grab url and decode it
+        url = ans["result"][0].decode()
+        # check if url begins with http(s) then redirect user to that site
+        if url.startswith("https://") or url.startswith("http://"):
+            return redirect(url)
+        else:
+            return redirect(f"https://{url}")
+    else:
+        # on failure, return the message
+        return ans["message"]
+
+# connect to the database and run a query
+def runDbQuery(query):
+    failure = {"status": False,
+        "message": "There was an issue processing you request."}
     try:
         # create connection to the database
         connection = mysql.connector.connect(
@@ -67,38 +95,36 @@ def receiveID(id):
             password=password,
             database=database,
         )
-        # prepare select statement, we know we want to turn the ID into a url
-        select_statment = f"SELECT longurl from {table} where hashid = {id}"
-        print(select_statment)
         # utilize prepared statements, returns obj of type MySQLCursorPrepared
         dbcursor = connection.cursor(prepared=True)
-        dbcursor.execute(select_statment)
-        # returns a tuple with url as a bytearray (url, '')
-        data = dbcursor.fetchone()
+
+        # check what statement needs to be run
+        if query.lower().startswith("select"):
+            dbcursor.execute(query)
+            # returns a tuple with url as a bytearray (url, '')
+            data = dbcursor.fetchone()
+            # let the user know if they entered a value that we don't have
+            if len(data) == 0:
+                ret = failure
+            else:
+                ret = {"status": True, "result": data}
+        elif query.lower().startswith("insert"):
+            dbcursor.execute(query)
+            connection.commit()
+            ret = {"status": True, "message": "Success"}
+
         # close database connections
         if (connection.is_connected()):
             dbcursor.close()
             connection.close()
             print("MySQL connection is closed")
-        # let the user know if they entered a value that we don't have
-        if len(data) == 0:
-            return "Invalid ID entered."
-        # grab url and decode it
-        url = data[0].decode()
-        print(url)
-        # check if url begins with http(s)
-        if url.startswith("https://") or url.startswith("http://"):
-            return redirect(url)
-        else:
-            return redirect(f"https://{url}")
+
+        # return to the calling function, returns a status and a message or the
+        # value pulled from the database
+        return ret
     except mysql.connector.Error as e:
         print(e)
-        return "There was an issue processing you request."
-    # finally:
-
-            # return ""
-            # return redirect("https://www.google.com")
-        return "Complete but no connection"
+        return failure
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
